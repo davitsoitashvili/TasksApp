926929574e5168ffb0bd21bd427eee6aca15b7be
Davit Soitashvili <davit.soitashvili.1@btu.edu.ge>
2021-01-19 13:41:30 +0400

    implement progress bar for tasks condition

--- src/components/GetTasksList.jsx
+++ src/components/GetTasksList.jsx
@@ -10,17 +10,76 @@ function GetTasksList(props) {
     dispatch(getTasksListAsync());
   }, [dispatch]);
 
+  var backLogPercent = 0;
+  var toDoPercent = 0;
+  var inProggressPercent = 0;
+  var donePercent = 0;
+
+  function countConditionsPercent() {
+    for (var i = 0; i < tasks.length; i++) {
+      switch (tasks[i].condition) {
+        case "Backlog":
+          backLogPercent++;
+          break;
+
+        case "To Do":
+          toDoPercent++;
+          break;
+
+        case "In Progress":
+          inProggressPercent++;
+          break;
+
+        case "Done":
+          donePercent++;
+          break;
+      }
+    }
+  }
+
+  countConditionsPercent()
+
   return (
-    <div>
+
+    <div
+      style={{
+        width: "300px",
+        marginLeft: "auto",
+        marginRight: "auto",
+        border: "1px solid black",
+        paddingBottom: "10px",
+        paddingTop: "10px",
+      }}
+    >
+      <div id="progress_bar_div">
+        <label for="Backlog">Backlog: </label>
+        <progress id="Backlog" value={backLogPercent} max="100"/>
+        <br />
+        <label for="To Do">To Do: </label>
+        <progress id="To Do" value={toDoPercent} max="100"/>
+        <br />
+        <label for="In Progress">In Progress: </label>
+        <progress id="In Progress" value={inProggressPercent} max="100"/>
+        <br />
+        <label for="Done">Done: </label>
+        <progress id="Done" value={donePercent} max="100"/>
+      </div>
+
       {tasks.map((task) => {
         return (
-          <div key={task.id}>
+          <div
+            key={task.id}
+            style={
+              ({ padding: "5px" },
+              { margin: "auto" },
+              { backgroundColor: "gray" })
+            }
+          >
             <h1>Title : {task.title}</h1>
-            <h2>Due To: {task.date_due}</h2>
+            <h2>Due To: {task.due_date}</h2>
             <h3>Assigned To : {task.assigned_to}</h3>
-            <h4>Comment : {task.comment}</h4>
-            <h5>Condition : {task.condition}</h5>
-            <hr />
+            <h3>Comment : {task.comment}</h3>
+            <h3>Condition : {task.condition}</h3>
           </div>
         );
       })}
